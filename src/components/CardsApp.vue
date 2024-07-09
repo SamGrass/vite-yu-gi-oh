487e78f99d67044a891ecf47d5adc44b9ad93d47
Samgrass <samuelegrassetti01@gmail.com>
2024-07-09 20:24:39 +0200

    creazione componente Card con relativi props

--- src/components/CardsApp.vue
+++ src/components/CardsApp.vue
@@ -1,12 +1,37 @@
 <script>
 export default {
-    name: 'CardsApp'
+    name: 'CardsApp',
+    props: {
+        image: String,
+        cardName: String,
+        type: String
+    }
 }
 </script>
 
 <template>
-    <div>
+    <div class="card">
+        <img :src="image" :alt="cardName">
+        <div class="txt-card-container">
+            <h4>{{ cardName }}</h4>
+            <div>{{ type }}</div>
+        </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.card {
+    flex-basis: calc(100% / 5 - 1rem);
+
+    .txt-card-container {
+        padding: 0.7rem;
+        background-color: $primary;
+
+        h4 {
+            color: #fff;
+        }
+    }
+}
+</style>
