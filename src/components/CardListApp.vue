7d941aef7ef02ae3626203bd4cb9b866d3bf32d4
Samgrass <samuelegrassetti01@gmail.com>
2024-07-09 19:31:39 +0200

    aggiunto markup a CardListApp

--- src/components/CardListApp.vue
+++ src/components/CardListApp.vue
@@ -7,9 +7,48 @@ export default {
 </script>
 
 <template>
-    <div>
-        adawda
-    </div>
+    <main>
+        <div class="container">
+            <!-- componente searchbar da aggiungere in futuro -->
+            afawfaf
+            <!-- card list -->
+            <div class="bg-white">
+                <div class="cards-found">Trovate tot carte</div>
+
+                <div class="cards-container">
+                    <!-- componente CardsApp da aggiungere -->
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+main {
+    background-color: $primary;
+    padding: 2rem 0;
+
+    .bg-white {
+        background-color: #fff;
+        padding: 2rem;
+        margin-top: 1rem;
+
+        .cards-found {
+            background-color: #212529;
+            padding: 0.5rem;
+            color: #fff;
+        }
+
+        .cards-container {
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            gap: 1.5rem;
+
+        }
+    }
+}
+</style>
