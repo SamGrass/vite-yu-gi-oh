0d7f91d4160fa970a0cf6e618615d6a321072bef
Samgrass <samuelegrassetti01@gmail.com>
2024-07-09 19:17:12 +0200

    aggiunto contenuto Header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -5,9 +5,26 @@ export default {
 </script>
 
 <template>
-    <div>
-        adadfggg
-    </div>
+    <header>
+        <a href="#" class="image-container">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg"
+                alt="logo">
+        </a>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+
+header {
+    padding: 1rem 2rem;
+    @include flex-center();
+
+    .image-container {
+        display: inline-block;
+        width: 200px;
+    }
+}
+</style>
