6d331369cd83ecb08c158e266e0a2cf868538d8f
Samgrass <samuelegrassetti01@gmail.com>
2024-07-11 21:11:45 +0200

    creata barra filtro in SearchbarApp

---
+++ src/components/SearchbarApp.vue
@@ -0,0 +1,24 @@
+<script>
+import {store} from '../store.js'
+
+export default {
+    name: 'SearchbarApp',
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <select v-model="store.archetypeActive" @change="$emit('selectArchetype')">
+            <option value="none">-- None --</option>
+            <option v-for="(archetype, index) in store.archetypeList" :key="index" :value="archetype.archetype_name">{{ archetype.archetype_name }}</option>
+        </select>
+    </div>
+</template>
+
+<style lang="scss" scoped> 
+</style>
